d49aadbfcaa784214c25816cfaf3c5741e54ee38
Özgür Akgün <ozgurakgun@gmail.com>
2018-07-10 00:17:09 +0100

    tidy recompute

--- experiment/recompute.sh
+++ experiment/recompute.sh
@@ -3,29 +3,41 @@ mkdir -p outputs
 mkdir -p logs
 mkdir -p plots
 
-# run the following command for the Minion
+
+# SAT
+parallel \
+    --joblog  logs/joblog-sat \
+    --results logs/results-sat \
+    --timeout 4h \
+    --eta \
+    "conjure solve {2} {1} --number-of-solutions all -o outputs/{2/.}-{3} --copy-solutions=no --solutions-in-one-file" \
+    ::: params/*.param \
+    ::: models/Langford-direct.essence models/Langford-positional.essence models/Langford-combined-symD-sdf.essence models/Langford-combined-symP-sdf.essence \
+    ::: bc_minisat_all nbc_minisat_all
+
+# Minion-static
 parallel \
     --joblog  logs/joblog-Minion \
     --results logs/results-Minion \
     --timeout 4h \
     --eta \
-    "conjure solve {2} {1} --number-of-solutions all -o outputs/{2/.} --copy-solutions=no --solutions-in-one-file" ::: params/*.param ::: models/*sdf.essence
+    "conjure solve {2} {1} --number-of-solutions all -o outputs/{2/.} --copy-solutions=no --solutions-in-one-file" \
+    ::: params/*.param \
+    ::: models/*.essence
 
-# parallel \
-#     --joblog  logs/joblog-Minion-wdegs \
-#     --results logs/results-Minion-wdegs \
-#     --timeout 4h \
-#     --eta \
-#     "conjure solve {2} {1} --number-of-solutions all -o outputs/{2/.}-{3} --copy-solutions=no --solutions-in-one-file --solver-options='-varorder {3}'" \
-#     ::: params/*.param \
-#     ::: models/Langford-direct.essence models/Langford-positional.essence \
-#     ::: wdeg domoverwdeg
-
-# and the following for the SAT
-# parallel --joblog joblog-sat --results results-sat --timeout 4h --eta -j16 "conjure solve Langford-{2}.essence {1} --solver bc_minisat_all --number-of-solutions all -o o-sat-{2} --copy-solutions=no --solutions-in-one-file" ::: params/*.param ::: direct positional combinedDirect
+# Minion-wdeg
+parallel \
+    --joblog  logs/joblog-Minion-wdegs \
+    --results logs/results-Minion-wdegs \
+    --timeout 4h \
+    --eta \
+    "conjure solve {2} {1} --number-of-solutions all -o outputs/{2/.}-{3} --copy-solutions=no --solutions-in-one-file --solver-options='-varorder {3}'" \
+    ::: params/*.param \
+    ::: models/Langford-direct.essence models/Langford-positional.essence \
+    ::: wdeg domoverwdeg
 
 # and following to generate the "all-info.txt" file
-# (cd outputs ; parallel -j1 --tag cat ::: */*info > all-info.txt)
+(cd outputs ; parallel -j1 --tag cat ::: */*info > all-info.txt)
 
 
 # this script will produce the tables and the plots
